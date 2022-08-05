a95fe798f24eb9c470ad92d6c07ab56517502df1
tituspijean <tituspijean@outlook.com>
2022-08-05 10:55:58 +0200

    Appease linter: install PHP dependencies with apt helper

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -2,15 +2,15 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies=""
-
 swap_needed=1024
 
+# PHP
 YNH_PHP_VERSION="7.3"
 YNH_COMPOSER_VERSION="2.0.13"
-
 extra_php_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-dom php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-pdo-mysql php${YNH_PHP_VERSION}-tokenizer php${YNH_PHP_VERSION}-zip"
+
+# dependencies used by the app
+pkg_dependencies="$extra_php_dependencies"
 
 # Version numbers
 project_version="1.4.0"
