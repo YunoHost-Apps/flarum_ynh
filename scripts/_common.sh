460fea18dfa03fbafd29d1c2bcd80f35dfd07c39
Patrick <49989644+botagas@users.noreply.github.com>
2025-03-21 17:31:36 +0200

    Update _common.sh

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -49,7 +49,7 @@ activate_flarum_extension() {
 #=================================================
 # EXPERIMENTAL HELPERS
 #=================================================
-ynh_add_swap_btrfs() {
+ynh_add_swap_compatibility() {
     if systemd-detect-virt --container --quiet; then
         ynh_print_warn --message="You are inside a container/VM. swap will not be added, but that can cause troubles for the app $app. Please make sure you have enough RAM available."
         return
